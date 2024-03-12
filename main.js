6a67e34d2d817b913d4d4016a791ab1ed2b96bff
ana-miau <163137224+ana-miau@users.noreply.github.com>
2024-03-12 09:39:44 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src ="https://static.wikia.nocookie.net/disney/images/5/5f/Aristocats-disneyscreencaps.com-3087.jpg/revision/latest/scale-to-width-down/276?cb=20200110181839&path-prefix=pt-br";
+}
+
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src ="https://miro.medium.com/v2/resize:fit:1400/0*zfNu_W38RW-KLOZm.jpg";
+}
